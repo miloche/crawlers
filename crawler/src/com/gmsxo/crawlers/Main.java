4956deb7f9c0c57a674ee4935b65cc0f0754349b
cybernetika <l0g1t3ch>
2012-12-24 00:37:07 +0100

    three errors

--- crawler/src/com/gmsxo/crawlers/Main.java
+++ crawler/src/com/gmsxo/crawlers/Main.java
@@ -1,3 +1,9 @@
+
+//there are three errors in this file The type 
+// java.lang.Object cannot be resolved. It is indirectly referenced from required .class files
+//Multiple markers at this line
+//	- Implicit super constructor Object() is undefined for default constructor. Must define an explicit 
+//	 constructor
 package com.gmsxo.crawlers;
 
 public class Main {
@@ -7,8 +13,8 @@ public class Main {
    */
   public static void main(String[] args) {
     
-  }
+  }  
 
 }
-
-//bomboclaut
+ 
+//b

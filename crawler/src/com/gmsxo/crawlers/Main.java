4d3b94584cf7082b797f396a7a4b8152d55102b3
miloche <miloche@atlas.cz>
2012-12-24 00:57:36 +0100

    MyCrawler updated - packages nad imports resolved Mian package resolved

--- crawler/src/com/gmsxo/crawlers/Main.java
+++ crawler/src/com/gmsxo/crawlers/Main.java
@@ -1,9 +1,3 @@
-
-//there are three errors in this file The type 
-// java.lang.Object cannot be resolved. It is indirectly referenced from required .class files
-//Multiple markers at this line
-//	- Implicit super constructor Object() is undefined for default constructor. Must define an explicit 
-//	 constructor
 package com.gmsxo.crawlers;
 
 public class Main {
